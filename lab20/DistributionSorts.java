3320191de2960dd2e31a83b7306baa8480a87f25
Hojung Lee <sandyhojunglee@berkeley.edu>
2019-08-08 12:57:46 -0700

    asfd

--- lab20/DistributionSorts.java
+++ lab20/DistributionSorts.java
@@ -3,14 +3,13 @@ public class DistributionSorts {
     /* Destructively sorts ARR using counting sort. Assumes that ARR contains
        only 0, 1, ..., 9. */
     public static void countingSort(int[] arr) {
-        // TODO: YOUR CODE HERE
 
         //keep track of how many of each index from 0-9 arr has
         int[] counts = new int[10];
 
         //at each arr's index, find what the value is, and add 1 to counts[arr's index value]
         for (int i = 0; i < arr.length; i++) {
-            counts[arr[i]] ++;
+            counts[arr[i]]++;
         }
 
         int[] sorted = new int[arr.length];
@@ -25,7 +24,7 @@ public class DistributionSorts {
 
         for (int arrIndex = arr.length - 1; arrIndex >= 0; arrIndex--) {
             sorted[starts[arr[arrIndex]]] = arr[arrIndex];
-            starts[arr[arrIndex]] ++;
+            starts[arr[arrIndex]]++;
         }
 
         //Destructive oohhhh
@@ -46,7 +45,8 @@ public class DistributionSorts {
        DIGIT-th digit. When DIGIT is equal to 0, sort the numbers by the
        rightmost digit of each number. */
     private static void countingSortOnDigit(int[] arr, int digit) {
-        // TODO: YOUR CODE HERE
+
+
     }
 
     /* Returns the largest number of digits that any integer in ARR has. */
