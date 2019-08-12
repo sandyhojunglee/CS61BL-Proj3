e800a36854e6ae866b6d75331d335206ae27d1fa
Annie <anniegao@berkeley.edu>
2019-08-12 13:03:07 -0700

    disjoint

--- lab21/UnionFind.java
+++ lab21/UnionFind.java
@@ -1,26 +1,59 @@
 public class UnionFind {
 
-    /* TODO: Add instance variables here. */
+    private int[] id;
+    private int[] size;
 
     /* Creates a UnionFind data structure holding N vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int N) {
-        // TODO: YOUR CODE HERE
+        id = new int[N];
+        size = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = -1;
+        }
+        for (int i = 0; i < N; i++) {
+            size[i] = 1;
+        }
     }
 
     /* Returns the parent of V. If V is the root of a tree, returns the
        negative size of the tree for which V is the root. */
     public int parent(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        //is this finding the root or just the parent above it
+        return id[v];
+    }
+
+    private int root(int v) {
+        while (id[v] >= 0) {
+            int temp = id[v];
+            v = id[v];
+            if (v < 0) {
+                return temp;
+            }
+        }
+        return v;
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. If invalid vertices are passed into this
        function, throw an IllegalArgumentException. */
     public int find(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        int root = id[v];
+        int temp = id[v];
+        while (root >= 0) {
+            temp = root;
+            root = id[root];
+        } //path compression
+        while (root(v) != v && root(v) != temp) {
+            int newPoint = id[v];
+            id[v] = root;
+            v = newPoint;
+        }
+//no path compression yet
+        if (temp < 0) {
+            return v;
+        }
+        return temp;
     }
 
     /* Connects two elements V1 and V2 together. V1 and V2 can be any element,
@@ -29,18 +62,35 @@ public class UnionFind {
        with itself or vertices that are already connected should not change the
        structure. */
     public void union(int v1, int v2) {
-        // TODO: YOUR CODE HERE
-    }
+        int v1Root = root(v1);
+        int v2Root = root(v2);
+
+        if (v1Root == v2Root) {
+            return;
+        }
+        if (size[v1Root] < size[v2Root]) {
+            id[v1Root] = v2Root;
+            size[v2Root] += size[v1Root];
+            id[v2Root] = -(size[v2Root]);
+        } else {
+            id[v2Root] = v1Root;
+            size[v1Root] += size[v2Root];
+            id[v1Root] = -(size[v1Root]);
+        }
+        //change all entries with wqupc[v1] to wqupc[v2]
+        //set ID of v1's root to the id of v2's root
+    } // every union calls find
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        int root = root(v);
+        return -(id[root]);
     }
 
     /* Returns true if nodes V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO: YOUR CODE HERE
-        return false;
+        return find(v1) == find(v2);
+        //check if v1 and v2 have the same root
+        //check if v1 and v2 have the same root/ "parent" ?
     }
 }
